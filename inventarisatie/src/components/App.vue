179242b2a0b748ef88a2522b126d6af2de1bc42a
Rémon Zander (0948619) <0948619@hr.nl>
2022-09-28 23:07:45 +0200

    added navbar but animation is not synced

--- inventarisatie/src/components/App.vue
+++ inventarisatie/src/components/App.vue
@@ -1,22 +1,19 @@
 <template>
     <div id="app">
-        <!--Sidebar with Dimmer -->
         <div class="fixed inset-0 flex z-40">
             <!-- Sidebar -->
             <div class="absolute flex top-0 h-screen z-20"
                  :class="[right ? 'right-0 flex-row' : 'left-0 flex-row-reverse']">
                 <!--Drawer -->
-                <button @click.prevent="toggle()"
-                        class="w-12 h-48 p-1 my-auto rounded text-white bg-gray-600 text-center focus:outline-none hover:bg-gray-500 transition-color duration-300">
-                    <span :class="[right ? '-rotate-90' : 'rotate-90']"
-                          class="block transform origin-center font-bold">
-                        Drawer
-                    </span>
-                </button>
+                <div class="w-10 h-full p-1 my-auto rounded-r-lg text-white bg-gray-600 text-center focus:outline-none hover:bg-gray-500 transition-color duration-300">
+                    <button @click.prevent="toggle()">
+                        <img src="@/assets/menu-icon.png" alt="menu">
+                    </button>
+                </div>
 
                 <!-- Sidebar Content -->
                 <div ref="content"
-                     class="transition-all duration-700 bg-yellow-200 overflow-hidden flex items-center justify-center"
+                     class="transition-all duration-700 bg-gray-600 overflow-hidden flex items-center justify-center"
                      :class="[open ? 'max-w-lg' : 'max-w-0']">
                     <div class="w-48 text-center font-bold text-xl">Sidebar</div>
                     <slot></slot>
@@ -25,8 +22,18 @@
         </div>
 
         <!-- Page Content -->
+
         <div class="absolute inset-1/2 rounded bg-green-500 w-1/2 h-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center text-white">
             Page Content
+        </div>
+
+        <div class="absolute bg-gray-200 top-0 right-0 transition-all duration-700" :class="[open ? 'left-[14.5rem]' : 'left-10']">
+            <!-- container navbar needed ? <nav class="container px-6 py-8 mx-auto md:flex md:justify-between md:items-center" /> -->
+            <div class="flex justify-between">
+                <router-link to="/" class="text-xl font-bold text-gray-800 md:text-2xl hover:text-blue-400">
+                    Logo
+                </router-link>
+            </div>
         </div>
     </div>
 </template>
@@ -64,4 +71,3 @@
         opacity: 0;
     }
 </style>
-
