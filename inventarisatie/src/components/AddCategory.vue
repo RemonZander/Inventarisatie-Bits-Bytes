7a5e9d9bf826d9b7ce0f708b85830a8f8e70ba06
Rémon Zander (0948619) <0948619@hr.nl>
2022-10-19 22:26:42 +0200

    busy adding table to add model page

--- inventarisatie/src/components/AddCategory.vue
+++ inventarisatie/src/components/AddCategory.vue
@@ -3,12 +3,52 @@
         <div class="flex flex-col gap-6">
             <p class="self-center text-[25px]">Categorie toevoegen</p>
             <div class="flex flex-row justify-between"><a>Naam:</a><input class="ml-[1vw]" type="text" id="NaamCategorie" placeholder="Naam categorie" /></div>
-            <div v-for="item in items">
-                <div class="flex flex-row justify-between"><a>{{ item.item }}</a><input class="ml-[1vw]" type="text" value="" /></div>
-            </div>
-            <div class="text-[18px] self-center">Eigenschap toevoegen</div>
-            <div class="flex flex-row justify-between"><a>Naam eigenschap:</a><input class="ml-[1vw]" type="text" id="NaamEigenschap" placeholder="Naam eigenschap" /></div>
-            <button @click.prevent="addItem()">toevoegen</button>
+
+
+            <table class="table text-black">
+                <thead>
+                    <tr>
+                        <th scope="col">Eigenschap</th>
+                        <th scope="col">DataType</th>
+                    </tr>
+                </thead>
+                <tbody v-for="eigenschap in Eigenschappen">
+                    <tr>
+                        <th scope="row"><input v-bind:id="'eigenschap' + eigenschap.pos" @keyup.enter="Saveproperty(eigenschap.pos)" type="text" class="placeholder-black bg-inherit text-black" v-model="eigenschap.eigenschap" /></th>
+                        <td>
+                            <div v-bind:id="'dropdown' + eigenschap.pos" class="dropdown">
+                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    {{eigenschap.datatype}}
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <a class="dropdown-item" v-on:click="SaveDataType(eigenschap.pos, 'text')">text</a>
+                                    <a class="dropdown-item" v-on:click="SaveDataType(eigenschap.pos, 'getal')">getal</a>
+                                    <a class="dropdown-item" v-on:click="SaveDataType(eigenschap.pos, 'datum')">datum</a>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+
+                <tbody>
+                    <tr>
+                        <th scope="row"><input id="eigenschap_new" @keyup.enter="Saveproperty(-1)" type="text" class="placeholder-black bg-inherit text-black" placeholder="Eigenschap toevoegen..." /></th>
+                        <td>
+                            <div id="dropdown_new" class="dropdown">
+                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    {{DropDownText}}
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <a class="dropdown-item" v-on:click="SaveDataType(-1, 'text')">text</a>
+                                    <a class="dropdown-item" v-on:click="SaveDataType(-1, 'getal')">getal</a>
+                                    <a class="dropdown-item" v-on:click="SaveDataType(-1, 'datum')">datum</a>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
             <button class="text-[20px] mt-[10vh]" @click.prevent="sendRequest()">Categorie toevoegen</button>
         </div>
     </div>
@@ -19,16 +59,39 @@
         name: "AddCategory",
         data() {
             return {
-                items: []
+                Eigenschappen: [{ pos: 0, eigenschap: "test123", datatype: "text" }],
+                DropDownText: "kies datatype...",
             };
         },
         methods: {
-            addItem() {
-                let value = document.getElementById('NaamEigenschap').value;
-                if (value != "") {
-                    this.items.push({ item: document.getElementById('NaamEigenschap').value });
-                    document.getElementById('NaamEigenschap').value = "";
+            Saveproperty(pos) {
+                if (pos === -1 && this.DropDownText === "kies datatype...") {
+                    return;
                 }
+                else if (pos === -1) {
+                    this.Eigenschappen.push({ pos: this.Eigenschappen[this.Eigenschappen.length - 1].pos + 1, eigenschap: document.getElementById("eigenschap_new").value, datatype: this.DropDownText });
+                    this.DropDownText = "kies datatype...";
+                    document.getElementById("eigenschap_new").value = "";                   
+                    console.log(this.Eigenschappen);
+                    return;
+                }
+                this.Eigenschappen[pos].eigenschap = document.getElementById("eigenschap" + pos).value;
+                console.log(this.Eigenschappen);
+            },
+            SaveDataType(pos, datatype) {
+                if (pos === -1 && document.getElementById("eigenschap_new").value === "") {
+                    this.DropDownText = datatype;
+                    return;
+                }
+                else if (pos === -1) {
+                    this.Eigenschappen.push({ pos: this.Eigenschappen[this.Eigenschappen.length - 1].pos + 1, eigenschap: document.getElementById("eigenschap_new").value, datatype: this.DropDownText });
+                    this.DropDownText = "kies datatype...";
+                    document.getElementById("eigenschap_new").value = "";
+                    console.log(this.Eigenschappen);
+                    return;
+                }
+                this.Eigenschappen[pos].datatype = datatype;
+                console.log(this.Eigenschappen);
             },
             async sendRequest() {
                 if (document.getElementById('NaamCategorie').value != "") {
