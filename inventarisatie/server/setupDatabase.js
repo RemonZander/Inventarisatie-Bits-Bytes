0148d775e1411372e59d38b936bf923da008f4a2
Rémon Zander (0948619) <0948619@hr.nl>
2022-11-21 22:56:04 +0100

    added data.sql file to setupdatabase.js

--- inventarisatie/server/setupDatabase.js
+++ inventarisatie/server/setupDatabase.js
@@ -1,13 +1,32 @@
 const db = require('./db')
 const fs = require("fs");
 
-const sqlData = fs
-    .readFileSync("database.db.sql", { flag: "r" })
-    .toString()
-    .trim()
-    .split(";");
+const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-for (var a = 0; a < sqlData.length - 1; a++) {
-    console.log(sqlData[a]);
-    db.db.run(sqlData[a]);
+async function Main() {
+    let sqlData = fs
+        .readFileSync("database.db.sql", { flag: "r" })
+        .toString()
+        .trim()
+        .split(";");
+
+    for (var a = 0; a < sqlData.length - 1; a++) {
+        console.log(sqlData[a]);
+        db.db.run(sqlData[a]);
+        await sleep(15);
+    }
+
+    sqlData = fs
+        .readFileSync("db_data.sql", { flag: "r" })
+        .toString()
+        .trim()
+        .split(";");
+
+    for (var a = 0; a < sqlData.length - 1; a++) {
+        console.log(sqlData[a]);
+        db.db.run(sqlData[a]);
+        await sleep(15);
+    }
 }
+
+Main();
