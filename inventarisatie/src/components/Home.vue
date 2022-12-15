a81b86c08f4cb3b97360264542190d351c4e3fe0
PhilipWu02 <philippewu2002@gmail.com>
2022-12-15 16:02:15 +0100

    delete button and index fix

--- inventarisatie/src/components/Home.vue
+++ inventarisatie/src/components/Home.vue
@@ -1,24 +1,17 @@
 <template>
-        <!-- <div class="flex">
-            <Sidebar />
-        <div class="flex flex-col w-full h-[100vh]" @click.prevent="toggle(true)">
-            <Menubar /> -->
-                <!-- Page Content -->
-                <div class="h-full">
-                    <!-- <router-view></router-view> -->
-                    <p class="h1">{{welcomeMessage}}</p> <!-- later in backend -->
+            <!-- Page Content -->
+                <div class="h-full" @click="Closepopup()">
                     <div class="dropdown">
                         <button class="btn btn-secondary dropdown-toggle" type="button" id="categoryButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             {{categoryText}}
                         </button>
-                        <!-- idee: het zichtbaar maken van op welk categorie je zit te kijken -->
-                        <ul class="dropdown-menu" aria-labelledby="categoryButton" @click="(showpopup = close)"> 
-                            <a class="dropdown-item" @click="createTableContent('Motherbord'), saveCategory('Moederborden')" href="#">Moederborden</a>
-                            <a class="dropdown-item" @click="createTableContent('CPUs'), saveCategory(`CPU's`)" href="#">CPU's</a>
-                            <a class="dropdown-item" @click="createTableContent('PSUs'), saveCategory(`Voedingen`)" href="#">Voedingen</a>
-                            <a class="dropdown-item" @click="createTableContent('PC_Cases'), saveCategory(`Kasten`)" href="#">Kasten</a>
+                        <ul class="dropdown-menu" aria-labelledby="categoryButton"> 
+                            <a class="dropdown-item" @click="createTableContent('Motherbord'), categoryText = 'Moederborden'" href="#">Moederborden</a>
+                            <a class="dropdown-item" @click="createTableContent('CPUs'), categoryText = `CPU's`" href="#">CPU's</a>
+                            <a class="dropdown-item" @click="createTableContent('PSUs'), categoryText = 'Voedingen'" href="#">Voedingen</a>
+                            <a class="dropdown-item" @click="createTableContent('PC_Cases'), categoryText = 'Kasten'" href="#">Kasten</a>
                             <a class="dropdown-item disabled" href="#">RAM</a>
-                            <a class="dropdown-item" @click="createTableContent('Coolers'), saveCategory(`Koelers`)" href="#">Koelers</a>
+                            <a class="dropdown-item" @click="createTableContent('Coolers'), categoryText = 'Koelers'" href="#">Koelers</a>
                             <a class="dropdown-item disabled" href="#">Uitbreidingskaarten</a>
                             <a class="dropdown-item disabled" href="#">HDD's en SDD's</a>
                             <a class="dropdown-item disabled" href="#">Switches</a>
@@ -26,7 +19,7 @@
                         </ul>
                     </div>
                     <div id="tableContent">
-                        <div class="overflow-y-scroll h-[75vh]">                   
+                        <div class="overflow-y-scroll h-[80vh]">                   
                         <table class="table-striped table-bordered w-full">
                             <thead class="bg-dark">
                             <tr>
@@ -36,9 +29,8 @@
                             <tbody>
                                 <tr v-for="(item, index) in tableData">                            
                                     <td scope="row" v-for="key in Object.keys(item)" :hidden="key === 'ID' ? true : false">{{tableData[index][key]}}</td>
-                                    <td><button id="showModal" @click="(showpopup = !showpopup), popupContent(item)"><img class="h-5 w-5" src="@/assets/editIcon.png"></button></td>
-                                    <div v-show="showpopup" class="flex flex-col bg-green-100 absolute border-solid border-4 border-black inset-1/2 w-[35vw] h-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                                        <!-- <p v-for="key in Object.keys(itemData)">{{key + ": " + itemData[key]}}</p> -->
+                                    <td><button id="showModal" @click="popupContent(item, index)"><img class="h-5 w-5" src="@/assets/editIcon.png"></button></td>
+                                    <div v-show="showpopup" class="flex flex-col bg-green-100 absolute border-solid border-4 border-black inset-1/2 w-[35vw] h-1/2 transform -translate-x-1/2 -translate-y-1/2" @click="popupInteraction = true;">
                                         <p class="flex flex-row justify-start" v-for="key in Object.keys(itemData)">
                                             <div class="flex flex-row items-center justify-end w-3/4 ml-[1vw]">
                                                 <span class="text-left">{{key + ": "}}</span>
@@ -46,12 +38,12 @@
                                             </div>
                                         </p>
                                         <div>
-                                            <button class="btn text-center absolute bottom-0 left-0 inset-x-1/2" @click="say('Dit heeft nog geen functie')">Verwijderen</button>
-                                            <button class="btn text-center absolute bottom-0 right-0 inset-x-1/2" @click="tableData[index] = itemData; showpopup = !showpopup">Opslaan</button>
+                                            <button class="btn text-center absolute bottom-0 left-0 inset-x-1/2" @click="removeItem()">Verwijderen</button>
+                                            <button class="btn text-center absolute bottom-0 right-0 inset-x-1/2" @click="saveItem()">Opslaan</button>
+                                            <!-- bevestigingsscherm nodig -->
                                         </div>
                                     </div>
                                     <!-- v-if checken -->
-                                    <!-- knop for acties (stuurt door naar een pagina met hetzelfde informatie over de item, met ook bewerken/verwijderen) -->
                                 </tr>
                             </tbody>
                         </table>
@@ -72,11 +64,12 @@
                 dimmer: true,
                 right: false,
                 logo: require('../assets/bits&bytes logo.jpg'),
-                welcomeMessage: 'Welkom',
                 tableData: [{test: "hoi"}],
                 itemData: [],
                 categoryText: 'kies categorie...',
-                showpopup: false
+                showpopup: false,
+                recordNumber: 0,
+                popupInteraction: false,
             };
         },
         methods: {
@@ -97,27 +90,36 @@
                     this.tableData = await response.json();
                     console.log(this.tableData);
             },
-            popupContent(item) {
+            popupContent(item, index) {
+                this.popupInteraction = true;
                 this.itemData = [];
                 const keys = Object.keys(item);
                 for (let index = 0; index < keys.length; index++) {
                     this.itemData[keys[index]] = item[keys[index]];
-                }             
+                };
+                this.recordNumber = index;  
+                this.showpopup = true;                   
             },
-            enableEdit(key) {
-                //document.getElementById(key).
+            saveItem(){
+                this.tableData[this.recordNumber] = this.itemData; 
+                this.showpopup = !this.showpopup; 
             },
-            saveCategory(categoryText) {
-                this.categoryText = categoryText;
-                console.log(this.categoryText)
+            Closepopup(){
+                if (this.showpopup && !this.popupInteraction){;
+                    this.showpopup = !this.showpopup;
+                }
+                else if (this.popupInteraction){
+                    this.popupInteraction = false;
+                }
+            },
+            removeItem(){
+                this.tableData.splice(this.recordNumber, 1);
+                this.showpopup = false;
+                this.itemData = [];
             },
             say(message) {
-    	        alert(message)
+    	        alert(message);
   	        },
-            // saveItem(index){
-            //     this.tableData[index] = this.itemData;
-            //     console.log(index);
-            // }
         },
     };
 </script>
